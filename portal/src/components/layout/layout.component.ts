dd23923be1c618b736a12c76a2053e5333e8618f
wisdman <wisdman@ajaw.it>
2017-12-15 05:51:07 +0700

    release build

--- portal/src/components/layout/layout.component.ts
+++ portal/src/components/layout/layout.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core'
+import { Component, ViewEncapsulation, PLATFORM_ID, Inject } from '@angular/core'
+import { isPlatformServer } from '@angular/common'
 import { Router, NavigationEnd } from '@angular/router'
 
 @Component({
@@ -9,15 +10,22 @@ import { Router, NavigationEnd } from '@angular/router'
 })
 export class LayoutComponent {
 
+  private isServer: boolean
+
   constructor(
+    @Inject(PLATFORM_ID) platformId: Object,
     private readonly _router: Router,
   ) {
+    this.isServer = isPlatformServer(platformId)
 
     this._router
         .events
         .filter( event => (event instanceof NavigationEnd) )
         .subscribe( event => {
-          console.log(event)
+          if (this.isServer)
+            return
+
+          window.scrollTo(0,0)
         })
 
   }
